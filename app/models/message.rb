0ff90ecc0c619c5f3397e63b30c3148fe2850939
Alexey Sukhoviy <martinsilenn@gmail.com>
2011-03-20 23:48:18 +0200

    plus colors

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,13 @@
 class Message < ActiveRecord::Base
-  
+
+  COLORS =
+  [
+  ["Red", "cc0000" ],
+  ["Yellow", "dddd00" ],
+  ["Green", "00cc00" ]
+  ]
   validates_presence_of :body
-  
+  validates :color, :inclusion => COLORS.map {|disp, value| value}
+
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110316144324) do
+ActiveRecord::Schema.define(:version => 20110320201222) do
 
   create_table "messages", :force => true do |t|
     t.string   "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "emotion",    :default => "neutral.gif"
+    t.string   "color",      :default => "ffffff"
   end
 
 end
