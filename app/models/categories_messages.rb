a5d3fb6172638e72d133c275383ee59069c5606f
Alexey Sukhoviy <martinsilenn@gmail.com>
2011-04-16 21:38:52 +0300

    jQuery for add/delete comments and Admin for Categories, but some problems with dependet destroy for categories

---
+++ app/models/categories_messages.rb
@@ -0,0 +1,4 @@
+class Categories_Message < ActiveRecord::Base
+  #belongs_to :category
+end
+
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
   has_and_belongs_to_many :messages
-  
+#  has_many :categories_messages
   def name
     # self["name_#{I18n.locale}".to_sym]
     case I18n.locale.to_s
@@ -10,6 +10,19 @@ class Category < ActiveRecord::Base
       self[:name_ru].blank? ? self[:name] : self[:name_ru]
     else
       self[:name]
-    end    
+    end
+  end
+
+  def name=(value)
+    # self["name_#{I18n.locale}".to_sym]
+    case I18n.locale.to_s
+    when "en"
+      self[:name] = value
+    when "ru"
+      self[:name_ru] = value
+    else
+      self[:name] = value
+    end
   end
 end
+
